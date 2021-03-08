8ba4136253163a34c49acd743f326be1e180bc2d
Alexandre MORGAN <alexandre.morgan.pro@gmail.com>
2021-03-08 18:20:54 -0500

    Implement routes GET one/all POST DELETE and PUT

--- app.js
+++ app.js
@@ -20,16 +20,43 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
 app.post('/api/sauces', (req, res, next) => {
     delete req.body._id;
-    const sauce = new Sauce({
-      ...req.body
-    });
+    const sauce = new Sauce(
+      req.body
+    );
     sauce.save()
       .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
       .catch(error => res.status(400).json({ error }));
-  });
+});
+
+app.put('/api/sauces/:id', (req, res, next) => {
+  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+  Sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+    Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
+});
+
+
 
 module.exports = app;
