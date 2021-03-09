31ccf9109fd1f40434929e2c56a0bc04054b5575
Alexandre MORGAN <alexandre.morgan.pro@gmail.com>
2021-03-09 10:56:57 -0500

    add controllers/routes for sauce/user + login/sign

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Sauce = require('./models/sauce');
 
-const app = express();
+const saucesRoutes = require('./routes/sauce');
+
+const userRoutes = require('./routes/user');
 
 // Connexion à la BDD mongoDB
 mongoose.connect('mongodb+srv://OPCR-admin:Jirafe44!!@opcr-project6-cours1.pn325.mongodb.net/OPCR-project6-cours1?retryWrites=true&w=majority',
@@ -11,6 +12,8 @@ mongoose.connect('mongodb+srv://OPCR-admin:Jirafe44!!@opcr-project6-cours1.pn325
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const app = express();
 
 // Header d'authorisation pour tous le monde
 app.use((req, res, next) => {
@@ -23,40 +26,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.post('/api/sauces', (req, res, next) => {
-    delete req.body._id;
-    const sauce = new Sauce(
-      req.body
-    );
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
-      .catch(error => res.status(400).json({ error }));
-});
-
-app.put('/api/sauces/:id', (req, res, next) => {
-  Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.delete('/api/sauces/:id', (req, res, next) => {
-  Sauce.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-    .catch(error => res.status(400).json({ error }));
-});
-
-app.get('/api/sauces/:id', (req, res, next) => {
-  Sauce.findOne({ _id: req.params.id })
-    .then(sauce => res.status(200).json(sauce))
-    .catch(error => res.status(404).json({ error }));
-});
-
-app.get('/api/sauces', (req, res, next) => {
-    Sauce.find()
-    .then(sauces => res.status(200).json(sauces))
-    .catch(error => res.status(400).json({ error }));
-});
-
-
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
