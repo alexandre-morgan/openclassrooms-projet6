741f3d0833727d838276335659982bd82c6bbb59
Alexandre MORGAN <alexandre.morgan.pro@gmail.com>
2021-03-11 12:13:46 -0500

    modify usersLike type in models

--- models/sauce.js
+++ models/sauce.js
@@ -11,8 +11,8 @@ const sauceSchema = mongoose.Schema({
   heat: { type: Number, required: true },
   likes: { type: Number },
   dislikes: { type: Number },
-  usersLiked: { type: [String] },
-  usersDisliked: { type: [String] },
+  usersLiked: [String],
+  usersDisliked: [String],
 });
 
 module.exports = mongoose.model('Sauce', sauceSchema);
